a08757584238895c4f8994d10e19a5222d1d420f
Sushant <sushantyejare2@gmail.com>
2025-01-28 14:33:49 +0530

    first commit

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import NavBar from "./NavBar";
+import { Box, Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { Outlet } from "react-router-dom";
+
+
+
+function App() {
+
+
+const [darkMode,setDarkMode]=useState(false);
+const palleteType = darkMode ? 'dark':'light'
+const theme = createTheme({
+  palette: {
+    mode: palleteType,
+    background:{
+      default:(palleteType==='light')?'#eaeaea':'#12121'
+    }
+  }
+});
+
+const toggleDarkMode=()=>{
+  setDarkMode(!darkMode);
+}
+
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+    <NavBar toggleDarkMode={toggleDarkMode} darkMode={darkMode}/>
+    <Box
+    sx={{
+      minHeight:'100vh',
+      background:darkMode
+      ?'radial-gradient(circle,#1e3aBa,#111B27)'
+      :'radial-gradient(circle,#baecf9,#f0f9ff)',
+      py:6
+    }}>
+    <Container maxWidth='xl' sx={{mt:8}}>
+       
+       <Outlet />
+             </Container>
+    </Box>
+    
+      </ThemeProvider>
+  )
+}
+
+export default App
